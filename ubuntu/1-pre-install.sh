89b2bc112f5e13fc3b6535eb405146bff6abee18
Suikan <26223147+suikan4github@users.noreply.github.com>
2021-06-29 08:19:26 +0900

    Add echo for each line to debug.

--- ubuntu/1-pre-install.sh
+++ ubuntu/1-pre-install.sh
@@ -67,30 +67,38 @@ if [ ${ERASEALL} -eq 1 ] ; then
 	# Assign specified space and rest of disk to the EFI and LUKS partition, respectively.
 	if [  ${ISEFI} -eq 1 ] ; then
 		# Zap existing partition table and create new GPT
+		echo "Initialize ${DEV} with GPT."
 		sgdisk --zap-all "${DEV}"
 		# Create EFI partition and format it
+		echo "Create an EFI partition on ${DEV}."
 		sgdisk --new=${EFIPARTITION}:0:+${EFISIZE} --change-name=${EFIPARTITION}:"EFI System"  --typecode=${EFIPARTITION}:ef00 "${DEV}"  
+		echo "Format the EFI parttion."
 		mkfs.vfat -F 32 -n EFI-SP "${DEV}${EFIPARTITION}"
 		# Create Linux partition
+		echo "Create a Linux partition on ${DEV}."
 		sgdisk --new=${CRYPTPARTITION}:0:0    --change-name=${CRYPTPARTITION}:"Linux LUKS" --typecode=${CRYPTPARTITION}:8309 "${DEV}"
 		# Then print them
 		sgdisk --print "${DEV}"
 	else
 		# Zap existing partition table
+		echo "Erase partition table of ${DEV}."
 		dd if=/dev/zero of=${DEV} bs=512 count=1
 		# Create MBR and allocate max storage for Linux partition
+		echo "Create a Linux partition on ${DEV} with MBR."
 		sfdisk ${DEV} <<HEREDOC
 2M,,L
 HEREDOC
 	fi	# if EFI firmware
 
 	# Encrypt the partition to install Linux
+	echo "Initialize ${DEV}${CRYPTPARTITION} as crypt partition"
 	printf %s "${PASSPHRASE}" | cryptsetup luksFormat --type=luks1 --key-file - --batch-mode "${DEV}${CRYPTPARTITION}"
 
 fi	# if erase all
 
 # ----- Open the LUKS partition -----
 # Open the crypt partition. 
+echo "Open a crypt partition ${DEV}${CRYPTPARTITION} as ${CRYPTPARTNAME}"
 printf %s "${PASSPHRASE}" | cryptsetup open -d - "${DEV}${CRYPTPARTITION}" ${CRYPTPARTNAME}
 
 # Check whether successful open. If mapped, it is successful. 
@@ -105,16 +113,21 @@ HEREDOC
 fi	# if crypt volume is unable to open
 
 # ----- Configure the LVM in LUKS volume -----
-# Create a Physical Volume and Volume Group, if first time
-if [ ! -e /dev/${VGNAME} ]; then
+# Create a Physical Volume and Volume Group, if not exist
+if [ -e /dev/${VGNAME} ]; then
+	echo "Volume group ${VGNAME} already exist. Skipped to create"
+else
+	echo "Initialize a physical volume on ${CRYPTPARTNAME}"
 	pvcreate /dev/mapper/${CRYPTPARTNAME}
+	echo "and then create Volume group ${VGNAME}."
 	vgcreate ${VGNAME} /dev/mapper/${CRYPTPARTNAME}
 fi # if /dev/volume-groupt not exist
 
 # Create a SWAP Logical Volume on VG, if it doesn't exist
 if [ -e /dev/mapper/${VGNAME}-${LVSWAPNAME} ] ; then 
-	echo "Swap volume already exist. Skipped to create" 1>&2
+	echo "Swap volume already exist. Skipped to create"
 else
+	echo "Create logical volume ${LVSWAPNAME} on ${VGNAME}."
 	lvcreate -L ${LVSWAPSIZE} -n ${LVSWAPNAME} ${VGNAME} 
 fi	# if /dev/mapper/swap volume already exit. 
 
@@ -128,6 +141,7 @@ Installation terminated.
 HEREDOC
 	return
 else
+	echo "Create logical volume ${LVROOTNAME} on ${VGNAME}."
 	lvcreate -l ${LVROOTSIZE} -n ${LVROOTNAME} ${VGNAME}
 fi	# if the root volun already exist
 
