d4b92dea4748a88b7bc7d7a020bbdf909462eb93
Suikan <26223147+suikan4github@users.noreply.github.com>
2021-06-29 09:58:03 +0900

    Add double quote the name in message

--- ubuntu/1-pre-install.sh
+++ ubuntu/1-pre-install.sh
@@ -35,7 +35,7 @@ if [ ${ERASEALL} -eq 1 ] ; then
 	read YESNO
 	if [ ${YESNO} != "Y" -a ${YESNO} != "y" ] ; then
 		cat <<HEREDOC 1>&2
-Check config.sh. The ERASEALL is ${ERASEALL}.
+Check config.sh. The variable ERASEALL is ${ERASEALL}.
 
 Installation terminated.
 HEREDOC
@@ -67,7 +67,7 @@ if [ ${ERASEALL} -eq 1 ] ; then
 	# Assign specified space and rest of disk to the EFI and LUKS partition, respectively.
 	if [  ${ISEFI} -eq 1 ] ; then
 		# Zap existing partition table and create new GPT
-		echo "...Initialize ${DEV} with GPT."
+		echo "...Initialize \"${DEV}\" with GPT."
 		sgdisk --zap-all "${DEV}"
 		# Create EFI partition and format it
 		echo "...Create an EFI partition on ${DEV}."
@@ -75,16 +75,16 @@ if [ ${ERASEALL} -eq 1 ] ; then
 		echo "...Format the EFI parttion."
 		mkfs.vfat -F 32 -n EFI-SP "${DEV}${EFIPARTITION}"
 		# Create Linux partition
-		echo "...Create a Linux partition on ${DEV}."
+		echo "...Create a Linux partition on \"${DEV}\"."
 		sgdisk --new=${CRYPTPARTITION}:0:0    --change-name=${CRYPTPARTITION}:"Linux LUKS" --typecode=${CRYPTPARTITION}:8309 "${DEV}"
 		# Then print them
 		sgdisk --print "${DEV}"
 	else
 		# Zap existing partition table
-		echo "...Erase partition table of ${DEV}."
+		echo "...Erase partition table of \"${DEV}\"."
 		dd if=/dev/zero of=${DEV} bs=512 count=1
 		# Create MBR and allocate max storage for Linux partition
-		echo "...Create a Linux partition on ${DEV} with MBR."
+		echo "...Create a Linux partition on \"${DEV}\" with MBR."
 		sfdisk ${DEV} <<HEREDOC
 2M,,L
 HEREDOC
@@ -98,13 +98,13 @@ fi	# if erase all
 
 # ----- Open the LUKS partition -----
 # Open the crypt partition. 
-echo "...Open a crypt partition ${DEV}${CRYPTPARTITION} as ${CRYPTPARTNAME}"
+echo "...Open a crypt partition ${DEV}${CRYPTPARTITION} as \"${CRYPTPARTNAME}\""
 printf %s "${PASSPHRASE}" | cryptsetup open -d - "${DEV}${CRYPTPARTITION}" ${CRYPTPARTNAME}
 
 # Check whether successful open. If mapped, it is successful. 
 if [ ! -e /dev/mapper/${CRYPTPARTNAME} ] ; then 
 	cat <<HEREDOC 1>&2
-***** ERROR : Cannot open LUKS volume ${CRYPTPARTNAME} on ${DEV}${CRYPTPARTITION}. *****
+***** ERROR : Cannot open LUKS volume "${CRYPTPARTNAME}" on ${DEV}${CRYPTPARTITION}. *****
 Check passphrase and config.txt
 
 Installation terminated.
@@ -118,9 +118,9 @@ vgdisplay -s ${VGNAME} &> /dev/null
 if  [ $? -eq 0 ] ; then		# is return value 0? ( exist ?)
 	echo "...Volume group ${VGNAME} already exist. Skipped to create"
 else
-	echo "...Initialize a physical volume on ${CRYPTPARTNAME}"
+	echo "...Initialize a physical volume on \"${CRYPTPARTNAME}\""
 	pvcreate /dev/mapper/${CRYPTPARTNAME}
-	echo "...And then create Volume group ${VGNAME}."
+	echo "...And then create Volume group \"${VGNAME}\"."
 	vgcreate ${VGNAME} /dev/mapper/${CRYPTPARTNAME}
 fi # if /dev/volume-groupt not exist
 
@@ -128,21 +128,21 @@ fi # if /dev/volume-groupt not exist
 if [ -e /dev/mapper/${VGNAME}-${LVSWAPNAME} ] ; then 
 	echo "...Swap volume already exist. Skipped to create"
 else
-	echo "...Create logical volume ${LVSWAPNAME} on ${VGNAME}."
+	echo "...Create logical volume \"${LVSWAPNAME}\" on \"${VGNAME}\"."
 	lvcreate -L ${LVSWAPSIZE} -n ${LVSWAPNAME} ${VGNAME} 
 fi	# if /dev/mapper/swap volume already exit. 
 
 # Create a ROOT Logical Volume on VG. 
 if [ -e /dev/mapper/${VGNAME}-${LVROOTNAME} ] ; then 
 	cat <<HEREDOC 1>&2
-***** ERROR : Logical volume ${VGNAME}-${LVROOTNAME} already exists. *****
+***** ERROR : Logical volume "${VGNAME}-${LVROOTNAME}" already exists. *****
 Check LVROOTNAME environment variable in config.txt.
 
 Installation terminated.
 HEREDOC
 	return
 else
-	echo "...Create logical volume ${LVROOTNAME} on ${VGNAME}."
+	echo "...Create logical volume \"${LVROOTNAME}\" on \"${VGNAME}\"."
 	lvcreate -l ${LVROOTSIZE} -n ${LVROOTNAME} ${VGNAME}
 fi	# if the root volun already exist
 
