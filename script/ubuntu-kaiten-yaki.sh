1c707ff903c2e6ce8f849526432fa1434b1a5100
Suikan <26223147+suikan4github@users.noreply.github.com>
2021-07-03 12:16:02 +0900

    Add appropriate indent to the heredocument of chroot

--- script/ubuntu-kaiten-yaki.sh
+++ script/ubuntu-kaiten-yaki.sh
@@ -116,38 +116,38 @@ function post_install_local() {
 	echo "...Chroot to ${TARGETMOUNTPOINT}."
 	# shellcheck disable=SC2086
 	cat <<- HEREDOC | chroot ${TARGETMOUNTPOINT} /bin/bash
-	# Mount the rest of partitions by target /etc/fstab
-	mount -a
+		# Mount the rest of partitions by target /etc/fstab
+		mount -a
 
-	# Set up the kernel hook of encryption
-	echo "...Installing cryptsetup-initramfs package."
-	apt -qq install -y cryptsetup-initramfs
+		# Set up the kernel hook of encryption
+		echo "...Installing cryptsetup-initramfs package."
+		apt -qq install -y cryptsetup-initramfs
 
-	# Prepare a key file to embed in to the ramfs.
-	echo "...Prepairing key file."
-	mkdir /etc/luks
-	dd if=/dev/urandom of=/etc/luks/boot_os.keyfile bs=4096 count=1 status=none
-	chmod u=rx,go-rwx /etc/luks
-	chmod u=r,go-rwx /etc/luks/boot_os.keyfile
+		# Prepare a key file to embed in to the ramfs.
+		echo "...Prepairing key file."
+		mkdir /etc/luks
+		dd if=/dev/urandom of=/etc/luks/boot_os.keyfile bs=4096 count=1 status=none
+		chmod u=rx,go-rwx /etc/luks
+		chmod u=r,go-rwx /etc/luks/boot_os.keyfile
 
-	# Add a key to the key file. Use the passphrase in the environment variable. 
-	echo "...Adding a key to the key file."
-	printf %s "${PASSPHRASE}" | cryptsetup luksAddKey -d - "${DEV}${CRYPTPARTITION}" /etc/luks/boot_os.keyfile
+		# Add a key to the key file. Use the passphrase in the environment variable. 
+		echo "...Adding a key to the key file."
+		printf %s "${PASSPHRASE}" | cryptsetup luksAddKey -d - "${DEV}${CRYPTPARTITION}" /etc/luks/boot_os.keyfile
 
-	# Add the LUKS volume information to /etc/crypttab to decrypt by kernel.  
-	echo "...Adding LUKS volume info to /etc/crypttab."
-	echo "${CRYPTPARTNAME} UUID=$(blkid -s UUID -o value ${DEV}${CRYPTPARTITION}) /etc/luks/boot_os.keyfile luks,discard" >> /etc/crypttab
+		# Add the LUKS volume information to /etc/crypttab to decrypt by kernel.  
+		echo "...Adding LUKS volume info to /etc/crypttab."
+		echo "${CRYPTPARTNAME} UUID=$(blkid -s UUID -o value ${DEV}${CRYPTPARTITION}) /etc/luks/boot_os.keyfile luks,discard" >> /etc/crypttab
 
-	# Putting key file into the ramfs initial image
-	echo "...Registering key file to the ramfs"
-	echo "KEYFILE_PATTERN=/etc/luks/*.keyfile" >> /etc/cryptsetup-initramfs/conf-hook
-	echo "UMASK=0077" >> /etc/initramfs-tools/initramfs.conf
+		# Putting key file into the ramfs initial image
+		echo "...Registering key file to the ramfs"
+		echo "KEYFILE_PATTERN=/etc/luks/*.keyfile" >> /etc/cryptsetup-initramfs/conf-hook
+		echo "UMASK=0077" >> /etc/initramfs-tools/initramfs.conf
 
-	# Finally, update the ramfs initial image with the key file. 
-	echo "...Upadting initramfs."
-	update-initramfs -uk all
+		# Finally, update the ramfs initial image with the key file. 
+		echo "...Upadting initramfs."
+		update-initramfs -uk all
 
-	# Leave chroot
+		# Leave chroot
 	HEREDOC
 
 	# Unmount all
--- script/void-kaiten-yaki.sh
+++ script/void-kaiten-yaki.sh
@@ -140,41 +140,41 @@ function post_install_local() {
 	echo "...Chroot to ${TARGETMOUNTPOINT}."
 	# shellcheck disable=SC2086
 	cat <<- HEREDOC | chroot ${TARGETMOUNTPOINT} /bin/bash
-	# Mount the rest of partitions by target /etc/fstab
-	mount -a
+		# Mount the rest of partitions by target /etc/fstab
+		mount -a
 
-	# Set up the kernel hook of encryption
-	echo "...Installing cryptsetup-initramfs package."
-	xbps-install -y lvm2 cryptsetup
+		# Set up the kernel hook of encryption
+		echo "...Installing cryptsetup-initramfs package."
+		xbps-install -y lvm2 cryptsetup
 
-	# Prepare a key file to embed in to the ramfs.
-	echo "...Prepairing key file."
-	mkdir /etc/luks
-	dd if=/dev/urandom of=/etc/luks/boot_os.keyfile bs=4096 count=1 status=none
-	chmod u=rx,go-rwx /etc/luks
-	chmod u=r,go-rwx /etc/luks/boot_os.keyfile
+		# Prepare a key file to embed in to the ramfs.
+		echo "...Prepairing key file."
+		mkdir /etc/luks
+		dd if=/dev/urandom of=/etc/luks/boot_os.keyfile bs=4096 count=1 status=none
+		chmod u=rx,go-rwx /etc/luks
+		chmod u=r,go-rwx /etc/luks/boot_os.keyfile
 
-	# Add a key to the key file. Use the passphrase in the environment variable. 
-	echo "...Adding a key to the key file."
-	printf %s "${PASSPHRASE}" | cryptsetup luksAddKey -d - "${DEV}${CRYPTPARTITION}" /etc/luks/boot_os.keyfile
+		# Add a key to the key file. Use the passphrase in the environment variable. 
+		echo "...Adding a key to the key file."
+		printf %s "${PASSPHRASE}" | cryptsetup luksAddKey -d - "${DEV}${CRYPTPARTITION}" /etc/luks/boot_os.keyfile
 
-	# Add the LUKS volume information to /etc/crypttab to decrypt by kernel.  
-	echo "...Adding LUKS volume info to /etc/crypttab."
-	echo "${CRYPTPARTNAME} UUID=$(blkid -s UUID -o value ${DEV}${CRYPTPARTITION}) /etc/luks/boot_os.keyfile luks,discard" >> /etc/crypttab
+		# Add the LUKS volume information to /etc/crypttab to decrypt by kernel.  
+		echo "...Adding LUKS volume info to /etc/crypttab."
+		echo "${CRYPTPARTNAME} UUID=$(blkid -s UUID -o value ${DEV}${CRYPTPARTITION}) /etc/luks/boot_os.keyfile luks,discard" >> /etc/crypttab
 
-	# Putting key file into the ramfs initial image
-	echo "...Registering key file to the ramfs"
-	echo 'install_items+=" /etc/luks/boot_os.keyfile /etc/crypttab " ' > /etc/dracut.conf.d/10-crypt.conf
+		# Putting key file into the ramfs initial image
+		echo "...Registering key file to the ramfs"
+		echo 'install_items+=" /etc/luks/boot_os.keyfile /etc/crypttab " ' > /etc/dracut.conf.d/10-crypt.conf
 
-	# Finally, update the ramfs initial image with the key file. 
-	echo "...Upadting initramfs."
-	xbps-reconfigure -fa
-	echo "...grub-mkconfig."
-	grub-mkconfig -o /boot/grub/grub.cfg
-	echo "...update-grub."
-	update-grub
+		# Finally, update the ramfs initial image with the key file. 
+		echo "...Upadting initramfs."
+		xbps-reconfigure -fa
+		echo "...grub-mkconfig."
+		grub-mkconfig -o /boot/grub/grub.cfg
+		echo "...update-grub."
+		update-grub
 
-	# Leave chroot
+		# Leave chroot
 	HEREDOC
 
 	# Unmount all
